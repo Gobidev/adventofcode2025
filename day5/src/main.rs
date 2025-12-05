3c126f02c9865a9e287784c9fc6f7dca57e24113
Adrian Groh <adrian.groh@t-online.de>
2025-12-05 13:26:25 +0100

    Clean up day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -32,39 +32,30 @@ fn part1(input: &ParsedInput) -> usize {
         .count()
 }
 
-fn ranges_overlap(range1: &(usize, usize), range2: &(usize, usize)) -> bool {
-    range1.0 <= range2.1 && range2.0 <= range1.1
-}
-
-fn merge_ranges(range1: &(usize, usize), range2: &(usize, usize)) -> (usize, usize) {
-    (
-        usize::min(range1.0, range2.0),
-        usize::max(range1.1, range2.1),
-    )
-}
-
-fn reduce_overlapping(ranges: &[(usize, usize)]) -> Vec<(usize, usize)> {
+fn elliminate_overlapping(ranges: &[(usize, usize)]) -> Vec<(usize, usize)> {
     let mut new_ranges = vec![];
     let mut current = ranges.to_vec();
     while new_ranges != current {
         new_ranges = current.to_vec();
-        'outer: for i1 in 0..current.len() {
+        for i1 in 0..current.len() {
             for i2 in i1 + 1..current.len() {
-                if ranges_overlap(&current[i1], &current[i2]) {
-                    current.push(merge_ranges(&current[i1], &current[i2]));
+                if current[i1].0 <= current[i2].1 && current[i2].0 <= current[i1].1 {
+                    current.push((
+                        usize::min(current[i1].0, current[i2].0),
+                        usize::max(current[i1].1, current[i2].1),
+                    ));
                     current.remove(i2);
                     current.remove(i1);
-                    break 'outer;
+                    break;
                 }
             }
         }
     }
-
     new_ranges
 }
 
 fn part2(input: &ParsedInput) -> usize {
-    reduce_overlapping(&input.0)
+    elliminate_overlapping(&input.0)
         .iter()
         .map(|(r0, r1)| (r1 - r0) + 1)
         .sum()
